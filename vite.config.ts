9615465439697d6b7ad25f1a3e361b92dbebc95f
AndyNoob <comfortableandy9082@gmail.com>
2026-04-20 14:16:55 -0700

    Add icons to extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,30 @@
-import {defineConfig} from 'vite';
-import webExtension from "vite-plugin-web-extension";
-import zipPack from "vite-plugin-zip-pack";
-import pkg from "./package.json";
-import manifest from "./manifest.json";
+  import {defineConfig} from 'vite';
+  import webExtension from "vite-plugin-web-extension";
+  import zipPack from "vite-plugin-zip-pack";
+  import pkg from "./package.json";
+  import manifest from "./manifest.json";
 
-export default defineConfig({
-  publicDir: false, // webExtension plugin does this already
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    minify: false
-  },
-  plugins: [
-    // viteStaticCopy({
-    //   targets: [
-    //     {
-    //       src: 'manifest.json',
-    //       dest: './'
-    //     }
-    //   ]
-    // }),
-    webExtension({
-      skipManifestValidation: true,
-      browser: "firefox",
-      manifest: () => {
-        return {
-          ...manifest,
-          version: pkg.version,
-          description: pkg.description
+  export default defineConfig({
+    publicDir: "icons",
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      minify: false
+    },
+    plugins: [
+      webExtension({
+        skipManifestValidation: true,
+        browser: "firefox",
+        manifest: () => {
+          return {
+            ...manifest,
+            version: pkg.version,
+            description: pkg.description
+          }
         }
-      }
-    }),
-    zipPack({
-      outFileName: `${pkg.name}.zip`
-    })
-  ]
-});
+      }),
+      zipPack({
+        outFileName: `${pkg.name}.zip`
+      })
+    ]
+  });
