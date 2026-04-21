9759ccaa6c59e10eb298a2bab21065f488c62fca
AndyNoob <comfortableandy9082@gmail.com>
2026-04-21 14:08:44 -0700

    Try to get chrome manifest generation working

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,32 @@
-  import {defineConfig} from 'vite';
-  import webExtension from "vite-plugin-web-extension";
-  import zipPack from "vite-plugin-zip-pack";
-  import pkg from "./package.json";
-  import manifest from "./manifest.json";
+import {defineConfig} from 'vite';
+import webExtension from "vite-plugin-web-extension";
+import zipPack from "vite-plugin-zip-pack";
+import pkg from "./package.json";
+import manifest from "./manifest.json";
 
-  export default defineConfig({
-    publicDir: "icons",
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true,
-      minify: false
-    },
-    plugins: [
-      webExtension({
-        skipManifestValidation: true,
-        browser: "firefox",
-        manifest: () => {
-          return {
-            ...manifest,
-            version: pkg.version,
-            description: pkg.description
-          }
+const browserType = process.env["BROWSER"] ?? "firefox";
+console.log(`compiling for ${browserType}`);
+
+export default defineConfig({
+  publicDir: "icons",
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: false
+  },
+  plugins: [
+    webExtension({
+      browser: browserType,
+      manifest: () => {
+        return {
+          ...manifest,
+          version: pkg.version,
+          description: pkg.description
         }
-      }),
-      zipPack({
-        outFileName: `${pkg.name}.zip`
-      })
-    ]
-  });
+      }
+    }),
+    zipPack({
+      outFileName: `${pkg.name}.zip`
+    })
+  ]
+});
