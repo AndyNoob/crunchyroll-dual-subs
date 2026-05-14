585c5a4ffb7af54064ed3b5d42ed3b9560f0daaf
AndyNoob <comfortableandy9082@gmail.com>
2026-05-14 02:05:40 -0700

    Hijack hardSubs in playback if croptix isn't found

--- src/monkey-patch-fetch.js
+++ src/monkey-patch-fetch.js
@@ -3,6 +3,8 @@ const originalFetch = window.fetch;
 window.fetch = async function ( input, init ) {
   const res = await originalFetch.apply(this, [ input, init ]);
 
+  if (!res.ok) return res;
+
   try {
     const url =
       input instanceof Request ? input.url :
@@ -12,9 +14,22 @@ window.fetch = async function ( input, init ) {
     if (url.includes("/playback/v3/")) {
       console.log("[dual-sub] playback hijacked");
       const clone = res.clone();
-      clone.json().then(data => {
-        dispatchExtensionEvent("playback", data);
-      });
+      const data = await clone.json();
+      dispatchExtensionEvent("playback", data);
+      if (typeof SubtitlesOctopus != "function") {
+        for (let [ key, value ] of Object.entries(data[ "hardSubs" ])) {
+          if (key === data[ "audioLocale" ]) {
+            value.url = data[ "hardSubs" ][ "none" ].url;
+            console.log(`[dual-sub] changed hard sub url of ${key}`);
+          }
+        }
+        const cleanBlob = new Blob([ JSON.stringify(data) ], { type: 'application/json' });
+        return new Response(cleanBlob, {
+          status: res.status,
+          statusText: res.statusText,
+          headers: res.headers
+        });
+      }
     }
     if (url.includes("content/v2/cms/objects")) {
       console.log("[dual-sub] manifest hijacked");
@@ -47,11 +62,14 @@ XMLHttpRequest.prototype.send = function ( ...args ) {
     try {
       if (url?.includes("/multiprofile")) {
         console.log("[dual-sub] profiles (XHR) hijacked");
-        dispatchExtensionEvent("profiles", JSON.parse(this.responseText));
+        const payload = JSON.parse(this.responseText);
+        dispatchExtensionEvent("profiles", payload);
       }
       if (url?.includes("/token")) {
         console.log("[dual-sub] token (XHR) hijacked");
-        dispatchExtensionEvent("token", JSON.parse(this.responseText));
+        const payload = JSON.parse(this.responseText);
+        window.__dualSubsAuth = `${ payload.token_type } ${ payload.access_token }`
+        dispatchExtensionEvent("token", payload);
       }
     } catch (e) {
       console.error("[dual-sub] failed to parse XHR", e);
